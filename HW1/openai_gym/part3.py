7287381226bc5fe576d6de96aaaf7d664b657b0e
danmalkaa <danmalka@mail.tau.co.il>
2022-03-12 20:19:42 +0200

    updated requirements.txt

--- HW1/openai_gym/part3.py
+++ HW1/openai_gym/part3.py
@@ -6,7 +6,7 @@ def run_episode(env, weights):
     total_reward = 0
     observation = env.reset()
     for i in range(200): # Redundant because v0 stops after 200 rounds / v1 after 500 rounds
-        env.render() # Display pop up window
+        # env.render() # Display pop up window
         action = 0 if np.matmul(weights, observation) < 0 else 1 # gets the action index; action space size is 2
         observation, reward, done, info = env.step(action)
         total_reward += reward
