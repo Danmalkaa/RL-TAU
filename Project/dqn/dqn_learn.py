0413745a3f814d692c2c16f18a4e6bf4ef7f92a9
Ofek <64634068+crossingfingers@users.noreply.github.com>
2022-05-20 08:32:44 +0300

    Update dqn_learn.py

--- Project/dqn/dqn_learn.py
+++ Project/dqn/dqn_learn.py
@@ -21,7 +21,7 @@ import torch.autograd as autograd
 from utils.replay_buffer import ReplayBuffer
 from utils.gym import get_wrapper_by_name
 
-STATS_SAVE_PATH = "/content/drive/MyDrive/RL homework/RL Project/project_stats"
+STATS_SAVE_PATH = "/content/drive/MyDrive/RL homework/RL Project/project_stats/"
 FILE_NAME ="stats_"+time.strftime("%Y%m%d-%H%M%S")+"_"+(''.join(random.choice(string.ascii_letters) for _ in range(5)))
 FULL_PATH  = STATS_SAVE_PATH+FILE_NAME+'.pkl'
 print("Saving file output to "+STATS_SAVE_PATH)
