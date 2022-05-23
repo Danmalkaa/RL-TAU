b4762c819d2ae4f374d51e9a1e8a84412ddbba69
Danmalkaa <45342726+Danmalkaa@users.noreply.github.com>
2022-05-24 02:40:46 +0300

    Update dqn_model.py

--- Project/dqn/dqn_model.py
+++ Project/dqn/dqn_model.py
@@ -18,10 +18,14 @@ class DQN(nn.Module):
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
         self.fc4 = nn.Linear(7 * 7 * 64, 512)
         self.fc5 = nn.Linear(512, num_actions)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+
+
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.conv3(x))
         x = F.relu(self.fc4(x.view(x.size(0), -1)))
         return self.fc5(x)
@@ -46,23 +50,70 @@ class DQN_RAM(nn.Module):
         return self.fc4(x)
 
 class D_Model(nn.Module):
-    def __init__(self, in_channels=4, num_actions=18):
-        """
-        Initialize a dynamic  network for guided exploration
-            in_features: number of features of input.
-            num_actions: number of action-value to output, one-to-one correspondence to action in game.
-        """
+    ''' In this network the input image is supposed to be 84x84 '''
+
+    def __init__(self, in_channels=4, num_actions=64):
         super(D_Model, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=7, stride=4, dilation=4) # TODO: ASK - change parameters?print(num_params)
-        self.conv2 = nn.Conv2d(32, 16, kernel_size=4, stride=1)
-        self.conv3 = nn.Conv2d(16, 1, kernel_size=3, stride=1)
-        # self.fc1 = nn.Linear(7200, 512)
-        # self.fc2 = nn.Linear(512, 7200)
-        self.fc3 = nn.Linear(100, 84 * 84)
+        self.emb_size = num_actions
+        self.ndf = 64
 
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
+        # Input 84x84x4
+        self.conv1 = nn.Conv2d(in_channels, self.ndf, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.ndf)
 
-        return self.fc3(x.view(x.size(0), -1)).view((-1,84,84))
+        # Input 42x42x64
+        self.conv2 = nn.Conv2d(self.ndf, int(self.ndf*1.5), kernel_size=3, bias=False)
+        self.bn2 = nn.BatchNorm2d(int(self.ndf*1.5))
+
+        # Input 20x20x96
+        self.conv3 = nn.Conv2d(int(self.ndf*1.5), self.ndf*2, kernel_size=3, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.ndf*2)
+        self.drop_3 = nn.Dropout2d(0.4)
+
+        # Input 10x10x128
+        self.conv4 = nn.Conv2d(self.ndf*2, self.ndf*4, kernel_size=3, padding=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(self.ndf*4)
+        self.drop_4 = nn.Dropout2d(0.5)
+
+        # Input 5x5x256
+        self.fc1 = nn.Linear(self.ndf*4*5*5, self.emb_size, bias=True)
+        # self.bn_fc = nn.BatchNorm1d(self.emb_size)
+
+    def forward(self, input):
+        e1 = F.max_pool2d(self.bn1(self.conv1(input)), 2)
+        x = F.leaky_relu(e1, 0.2, inplace=True)
+        e2 = F.max_pool2d(self.bn2(self.conv2(x)), 2)
+        x = F.leaky_relu(e2, 0.2, inplace=True)
+        e3 = F.max_pool2d(self.bn3(self.conv3(x)), 2)
+        x = F.leaky_relu(e3, 0.2, inplace=True)
+        x = self.drop_3(x)
+        e4 = F.max_pool2d(self.bn4(self.conv4(x)), 2)
+        x = F.leaky_relu(e4, 0.2, inplace=True)
+        x = self.drop_4(x)
+        x = x.view(-1, self.ndf*4*5*5)
+        output = self.fc1(x)
+
+        return output
+
+
+# class D_Model(nn.Module):
+#     def __init__(self, in_channels=4, num_actions=18):
+#         """
+#         Initialize a dynamic  network for guided exploration
+#             in_features: number of features of input.
+#             num_actions: number of action-value to output, one-to-one correspondence to action in game.
+#         """
+#         super(D_Model, self).__init__()
+#         self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=7, stride=4, dilation=4) # TODO: ASK - change parameters?print(num_params)
+#         self.conv2 = nn.Conv2d(32, 16, kernel_size=4, stride=1)
+#         self.conv3 = nn.Conv2d(16, 1, kernel_size=3, stride=1)
+#         # self.fc1 = nn.Linear(7200, 512)
+#         # self.fc2 = nn.Linear(512, 7200)
+#         self.fc3 = nn.Linear(100, 84 * 84)
+
+#     def forward(self, x):
+#         x = F.relu(self.conv1(x))
+#         x = F.relu(self.conv2(x))
+#         x = F.relu(self.conv3(x))
+
+#         return self.fc3(x.view(x.size(0), -1)).view((-1,84,84))
